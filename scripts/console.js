e8abc918f86013b746778beedca7f00d8821ad67
unclelongmao <unclelongmao@gmail.com>
2014-04-23 17:11:02 +0800

    add console messgae

---
+++ scripts/console.js
@@ -0,0 +1,9 @@
+if (window.console) {
+    var cons = console;
+    if (cons) {
+        cons.log("%c\n      ", "font-size:60px;background:url('http://ippotsuko.com/console.png') no-repeat center");
+
+        cons.log('Love U，Nancy！');
+
+    }
+}
