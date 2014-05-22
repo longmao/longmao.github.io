766faa4baf2b850f5e98d963ed0844a7ccdddc18
unclelongmao <unclelongmao@gmail.com>
2014-05-22 09:37:21 +0800

    add bitbucket

--- scripts/console.js
+++ scripts/console.js
@@ -2,8 +2,6 @@ if (window.console) {
     var cons = console;
     if (cons) {
         cons.log("%c\n      ", "font-size:60px;background:url('http://ippotsuko.com/console.png') no-repeat center");
-
         cons.log('Love U，Nancy！');
-
     }
 }
