fa659a17dd5dc3d3690699a7e40c179245f82cd2
vincent.yang <vincent.yang@yeahmobi.com>
2016-12-29 11:48:53 +0800

    update

--- scripts/console.js
+++ scripts/console.js
@@ -1,13 +1,13 @@
 if (window.console) {
     var cons = console;
-    var condition = "jobhunting"
+    var condition = ""
     if (cons) {
         switch (condition) {
             case "jobhunting":
                 cons.log("如贵公司需要web前端工程师，可联系我: unclelongmao@gmail.com ，Email随时在线，最近正在换工作中，期待您的真挚来信")
                 break;
             default:
-                cons.log("%c\n       ", "font-size:60px;background:url('http://ippotsuko.com/console.png') no-repeat center");
+                cons.log("%c\n       ", "font-size:60px;background:url('https://pbs.twimg.com/profile_images/688766010837446657/2DgfpAQ6.png') no-repeat center");
                 cons.log('Love U，Nancy！');
         }
 
